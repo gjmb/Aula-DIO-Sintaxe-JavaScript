2a45b7731f93651e96a9f30ec0eabeda2a790e10
gjmb <gjmbarbosa@gmail.com>
2022-03-28 18:28:08 -0300

    Adiciona exercicios realizados

--- aula 5 e 6 - funções /script.js
+++ aula 5 e 6 - funções /script.js
@@ -20,6 +20,9 @@ function fatorial(number){
 
 console.log(fatorial(4));
 
+
+/*Exercicio: Fazer uma função que recebe dois números e diz se eles são iguais, > ou < que 10 e > ou < que 20 */
+
 function comparaNumeros(n1,n2){
     return n1==n2 ? `Os números ${n1} e ${n2} são iguais. `:`Os números ${n1} e ${n2} não são iguais. `;
 }
@@ -46,7 +49,7 @@ function f(n1, n2){
     if(soma < 10)
         return comparaNumeros(n1,n2) +'Sua soma é ' + soma + ', que é menor que 10 e menor 20.';
     
-        return comparaNumeros(n1,n2) +'Sua soma é ' + soma + ', que é igual a 10 e menor 20.'; 
+return comparaNumeros(n1,n2) +'Sua soma é ' + soma + ', que é igual a 10 e menor 20.'; 
 
 
     
@@ -55,5 +58,66 @@ function f(n1, n2){
 
 var a =9;
 var b='a';
-var r=f(a, b);
+var r=f();
 console.log(r);
+
+
+/*Exercicio: verifica se palavra é palindromo */
+
+function verificaPalindromo1(p1){
+    if(!p1)
+        return `Valores Inválidos.`
+    
+    return p1.split("").reverse().join("")==p1 ? `É um palindromo.` : `Não é um palindromo.`;
+}
+
+console.log(verificaPalindromo1("anilin"));
+
+function verificaPalindromo2(p1){
+    if(!p1)
+        return `Valores Inválidos.`
+    let leng = p1.length-1;
+    let palindromo=true;
+    for (let index = 0; index < p1.length/2; index++) {
+        if(p1[index]==p1[leng]){
+            leng--;
+            continue;
+        } else {
+            palindromo=false;
+            break;
+        }
+    }
+   
+    if(palindromo)
+        return `É um palindromo.`        
+    else
+        return `Não é um palindromo.`
+}
+
+console.log(verificaPalindromo2("anilina"));
+
+/*Exercicio: substitui elementos pares de um vetor por zero */
+
+let ar = [0,1,2,3,4,5,6,7,8];
+
+function substituiPares(vet){
+    if(!ar || vet==0)
+        return -1;
+
+    for (let index = 0; index < vet.length; index++) {
+        if(vet[index]%2===0)
+            vet[index]=0;        
+    }
+
+    return 1;
+}
+
+console.log(substituiPares(ar));
+console.log(substituiPares([]));
+
+console.log(ar);
+
+var obj ={
+    chave: 6
+}
+
